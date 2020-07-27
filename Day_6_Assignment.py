48f5e43044795b7532cceaec5c8cbe6904191198
saideepreddy003 <68642169+saideepreddy003@users.noreply.github.com>
2020-07-27 22:25:32 +0530

    Add files via upload

---
+++ Day_6_Assignment.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[25]:
+
+
+list1=[1,2,3,4,5,7,8]
+list2=["a","b","c","d","e"]
+len1=min(len(list1),len(list2))
+dict={}
+
+
+# In[12]:
+
+
+list(zip(list1,list2))
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
